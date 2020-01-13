7293f2866d49e2db1a00d4091de39a30b1ee658b
Fluffycat9999 <50632021+Fluffycat9999@users.noreply.github.com>
2020-01-13 03:40:08 -0500

    Hw#2

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+
+const members = require('./Member');
+const app = express();
+const logger = require('./middleware/logger');
+//Init middleware
+ app.use(logger);
+/* app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+}); */
+
+/* app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});  */
+
+
+//Gets all members
+app.get('/api/members', (req, res) =>
+    {res.json(members)}
+);
+
+//get single member
+app.get('/api/members/:id', (req, res) => {
+    /* res.send(req.params.id); */
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if(found){
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    }else{
+        res.status(400).json({msg: `No member with the Id of ${req.params.id}`});
+    }
+})
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>  console.log(`Server started on port ${PORT}`));
+//37:46
