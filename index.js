d22f1e609c7d2a754edfd548dd90acb1e56dfd6e
Fluffycat9999 <50632021+Fluffycat9999@users.noreply.github.com>
2020-01-13 18:49:57 -0500

    Hw#2

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const path = require('path');
-
+const exphbs = require('express-handlebars');
 const members = require('./Member');
 const app = express();
 const logger = require('./middleware/logger');
+
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members
+  })
+);
+
 //Init middleware
  app.use(logger);
 /* app.get('/', (req, res) => {
